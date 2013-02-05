b7991c5e46c172b35b82fe84a94039141de4378e
Janusz A. Urbanowicz <alex@hell.pl>
2013-02-05 23:54:40 +0100

    uurl shortener model

--- url/models.py
+++ url/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from profile.models import User
+
 # Create your models here.
+
+class URL(models.Model):
+  
+  active = models.BooleanField(default=True)
+  owner = models.ForeignKey(User)  
+  url = models.URLField(verify_exists=True)   
+  slug = models.SlugField(unique=True)
+  public = models.BooleanField()
+  description = models.TextField(blank=True)
+  created = models.DateTimeField(auto_now_add=True)
+          
+
+class Click (models.Model):
+
+  date = models.DateTimeField(auto_now_add=True)
+  ip = models.IPAddressField()
+  info = models.TextField()
+  user = models.ManyToManyField(User)
+
+
+# TODO tags            
