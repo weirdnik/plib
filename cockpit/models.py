cdd632787b0fa13c3bbc7ce41d785a159c92414a
Alex <alex@plum.me>
2013-02-20 15:14:13 +0100

    mentions re added

--- cockpit/models.py
+++ cockpit/models.py
@@ -8,6 +8,7 @@ from django.forms import ModelForm
 #
 
 TAG_RE = re.compile('#(?P<tag>\w+)')
+MENTION_RE = re.compile('^(?P<username>\w+)')
 
 # Create your models here.
 
@@ -25,28 +26,27 @@ class Status (models.Model):
 
   def render (self):
 
-    result = self.text
+    result = MENTION_RE.sub( lambda g: '<a href="%s">%s</a>' % (reverse('cockpit.views.main', kwargs=dict(username=g.group().strip('^'))), g.group().strip('^')), self.text))
   
     if self.tagged:
 
-      result = TAG_RE.sub ( lambda g: '<a href="%s">%s</a>' % (reverse('cockpit.views.tag', kwargs=dict(tag=g.group().strip('#'))) ,g.group()),  s))
-      
-    
-    return result
-  
-     
-  
+      result = TAG_RE.sub ( lambda g: '<a href="%s">%s</a>' % (reverse('cockpit.views.tag', kwargs=dict(tag=g.group().strip('#'))) ,g.group()),  s))    
+
+      return result
+
   
 class StatusForm (ModelForm):
   class Meta:
     model = Status
     fields = ['text', 'image']
 
+
 class Tag (models.Model):
 
   tag = models.TextField ()
   status = models.ManyToManyField (Status)
   
+
 class Like (models.Model):
 
   user = models.OneToOneField (User)
