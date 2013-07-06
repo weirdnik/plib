d9553b66902f84be39889362d7571808e45ff032
Alex <alex@plum.me>
2013-07-06 21:58:15 +0100

    front page

--- main/views.py
+++ main/views.py
@@ -7,16 +7,18 @@ from django.template import Context, loader
 
 from cockpit.models import Status
 
+from settings import APP_NAME
+
 def front(request):
 
+  print request.user.id
   if request.user.id:
     return HTTPResponseRedirect(reverse('cockpit.views.main'))
   else:
-    statuses = Status.objects.filter(private__exact=False, recipient__exact=False).order_by('-date')
+    statuses = Status.objects.filter(private__exact=False, recipient__exact=None).order_by('-date')
     
-    if statuses:
-      feed = statuses[:20] if statuses else None
+    feed = statuses[:20] if statuses else None
     
     template = loader.get_template('front.html')
-    return HTTPResponse(template.render(Context(dict(feed=feed))))
+    return HTTPResponse(template.render(Context(dict(feed=feed,app_name=APP_NAME))))
       
