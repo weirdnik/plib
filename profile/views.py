8a5a814673bece8c3c48360404ae17686f4dab92
Maciej Orliński <maciejorlinski1@gmail.com>
2013-08-06 14:18:52 +0200

    pierwsze podejscie do wizytowki. O to chodzilo mniej wiecej?

--- profile/views.py
+++ profile/views.py
@@ -2,7 +2,7 @@
 
 import string, random
 
-from PIL import Image
+from Image import Image
 
 import sendmail
 
@@ -39,19 +39,19 @@ def follow (request, username):
 
   if request.method == 'POST':
   
-    user =  get_object_or_404(User, user__id__exact=request.user.id)  
-    follow = get_object_or_404 (User, user__username__exact=username)
+	user =	get_object_or_404(User, user__id__exact=request.user.id)  
+	follow = get_object_or_404 (User, user__username__exact=username)
   
-    user.watches.add(follow)
-    user.save()
+	user.watches.add(follow)
+	user.save()
 
-    action = Status(owner=follow, recipient=user, action='follow')
-    action.save()
-    
+	action = Status(owner=follow, recipient=user, action='follow')
+	action.save()
+	
   
-    return HTTPResponseRedirect (reverse('mobile_dashboard'))
+	return HTTPResponseRedirect (reverse('mobile_dashboard'))
   else:
-    return HTTPResponseNotAllowed ()
+	return HTTPResponseNotAllowed ()
 
 
 @login_required
@@ -59,30 +59,45 @@ def unfollow (request, username):
 
   if request.method == 'POST':
   
-    user =  get_object_or_404(User, user__id__exact=request.user.id)
-    follow = get_object_or_404 (User, user__username__exact=username)
+	user =	get_object_or_404(User, user__id__exact=request.user.id)
+	follow = get_object_or_404 (User, user__username__exact=username)
 
-    if follow in user.watches.all():
-      user.watches.remove(follow)
-      user.save()
+	if follow in user.watches.all():
+	  user.watches.remove(follow)
+	  user.save()
 
-    action = Status(owner=follow, recipient=user, action='unfollow')
-    action.save()
+	action = Status(owner=follow, recipient=user, action='unfollow')
+	action.save()
   
-    return HTTPResponseRedirect (reverse('mobile_dashboard'))
+	return HTTPResponseRedirect (reverse('mobile_dashboard'))
   else:
-    return HTTPResponseNotAllowed ()
+	return HTTPResponseNotAllowed ()
 
 
 def blog (request, username):
   if request.method == 'GET':
   
-    user =  get_object_or_404(User, user__username__exact=username)  
-    statuses = Status.objects.filter(owner__exact=user, recipient__exact=None)
+	user =	get_object_or_404(User, user__username__exact=username)
+	userinfo = User.objects.filter(user__username__exact=username)
+	if userinfo[0].about:
+		about = userinfo[0].about
+	else:
+		about = ""
+	if userinfo[0].icbm:
+		icbm = userinfo[0].icbm
+	else:
+		icbm = ""
+	avatar = userinfo[0].avatar
+	if userinfo[0].phone:
+		phone = userinfo[0].phone
+	else:
+		phone = ""
+	
+	statuses = Status.objects.filter(owner__exact=user, recipient__exact=None)
 
-    template = loader.get_template('blog.html')
-    
-    return HTTPResponse(template.render(Context(dict(statuses=statuses, profile=user))))
+	template = loader.get_template('blog.html')
+	
+	return HTTPResponse(template.render(Context(dict(statuses=statuses, profile=user, about=about, icbm=icbm, phone=phone, avatar=avatar))))
 
 
 def register (request):
@@ -90,110 +105,110 @@ def register (request):
   template = loader.get_template ('register.html')
 
   class RegistrationForm(UserCreationForm):
-#    pass
-    email = forms.EmailField(max_length=128)
-       
+#	 pass
+	email = forms.EmailField(max_length=128)
+	   
   if request.method == 'GET':
 
-    return HTTPResponse (template.render(RequestContext(request, dict(form=RegistrationForm()))))
-    
+	return HTTPResponse (template.render(RequestContext(request, dict(form=RegistrationForm()))))
+	
   elif request.method == 'POST':
 
-    form = RegistrationForm(request.POST)
-    
-    if form.is_valid():
-    
-      print  dir(form['password1']),  form['password2'].data
-      if  form['password1'].data == form['password2'].data:
-        username = form['username'].data.strip()
-        password = form['password1'].data
-        email = form['email'].data
-         
-        # Warning: elsewhere in the code user is profile.models.User instance
-        # here it is django.auth User instance
+	form = RegistrationForm(request.POST)
+	
+	if form.is_valid():
+	
+	  print	 dir(form['password1']),  form['password2'].data
+	  if  form['password1'].data == form['password2'].data:
+		username = form['username'].data.strip()
+		password = form['password1'].data
+		email = form['email'].data
+		 
+		# Warning: elsewhere in the code user is profile.models.User instance
+		# here it is django.auth User instance
 
-        try: 
-          user = DjangoUser.objects.get(username=username)        
-        except DjangoUser.DoesNotExist:
-          user = DjangoUser.objects.create_user(username, email, password)
-          user.is_active = False
-          user.save()
-                  
-          profile = User(user=user)
-          slug = ''
-          for i in xrange(16):
-            slug = slug + random.choice(string.ascii_letters)
-          profile.slug = slug
-          
-          slug_path = reverse('profile.views.confirm', kwargs=dict(slug=slug))
-          slug_url = request.META.get('HTTP_ORIGIN',HOST_URL) + slug_path
-         
-          profile.save()
-          sendmail.confirm (email, slug_url)          
-          template = loader.get_template ('confirm.html')
-          return HTTPResponse (template.render(RequestContext(request, dict(email=email))))
-      else:
-        print 'bad passwords'
-        return HTTPResponseBadRequest()
-    else:
-      return HTTPResponse (template.render(RequestContext(request, dict(form=form))))
+		try: 
+		  user = DjangoUser.objects.get(username=username)		  
+		except DjangoUser.DoesNotExist:
+		  user = DjangoUser.objects.create_user(username, email, password)
+		  user.is_active = False
+		  user.save()
+				  
+		  profile = User(user=user)
+		  slug = ''
+		  for i in xrange(16):
+			slug = slug + random.choice(string.ascii_letters)
+		  profile.slug = slug
+		  
+		  slug_path = reverse('profile.views.confirm', kwargs=dict(slug=slug))
+		  slug_url = request.META.get('HTTP_ORIGIN',HOST_URL) + slug_path
+		 
+		  profile.save()
+		  sendmail.confirm (email, slug_url)		  
+		  template = loader.get_template ('confirm.html')
+		  return HTTPResponse (template.render(RequestContext(request, dict(email=email))))
+	  else:
+		print 'bad passwords'
+		return HTTPResponseBadRequest()
+	else:
+	  return HTTPResponse (template.render(RequestContext(request, dict(form=form))))
 
 def confirm (request, slug=None):
 
   if slug:
-    user = get_object_or_404(User, slug__exact=slug)
-    if user:
-      user.user.is_active = True
-      user.user.save()
-      user.active = True
-      user.save()
-#      u = authenticate(user.user.username, password)        
-#      login (request, user.user)
-      template = loader.get_template('confirmed.html')
-      
-      return HTTPResponse(template.render(Context(dict(user=user))))
-      
+	user = get_object_or_404(User, slug__exact=slug)
+	if user:
+	  user.user.is_active = True
+	  user.user.save()
+	  user.active = True
+	  user.save()
+#	   u = authenticate(user.user.username, password)		 
+#	   login (request, user.user)
+	  template = loader.get_template('confirmed.html')
+	  
+	  return HTTPResponse(template.render(Context(dict(user=user))))
+	  
   return HTTPResponseRedirect(reverse('cockpit.views.main'))
 
 
 def edit (request):
 
   def process_images(**kwargs):
-    import os
-    
-    instance = kwargs.get('instance', None)
-    if instance.avatar:
-      avatar = Image.open(instance.avatar.path)
-      avatar.thumbnail((256,256), Image.ANTIALIAS)
-      avatar.save(instance.avatar.path + '.jpg', 'JPEG')
+	import os
+	
+	instance = kwargs.get('instance', None)
+	if instance.avatar:
+	  avatar = Image.open(instance.avatar.path)
+	  avatar.thumbnail((256,256), Image.ANTIALIAS)
+	  avatar.save(instance.avatar.path + '.jpg', 'JPEG')
 
   # works but unnecessary
   
-#    if instance.background:
-#      background = Image.open(instance.background.path)
-#      path = os.path.join((lambda p:'/'.join(p.split('/')[:-1]))(instance.background.path), 'background-%s.jpg' % instance.user.username)
-#      background.save(path,'JPEG')
-      
+#	 if instance.background:
+#	   background = Image.open(instance.background.path)
+#	   path = os.path.join((lambda p:'/'.join(p.split('/')[:-1]))(instance.background.path), 'background-%s.jpg' % instance.user.username)
+#	   background.save(path,'JPEG')
+	  
   post_save.connect(process_images, sender=User)
-  template = loader.get_template ("account.html")                                                                          
-  user =  get_object_or_404(User, user__id__exact=request.user.id)  
+  template = loader.get_template ("account.html")																		   
+  user =  get_object_or_404(User, user__id__exact=request.user.id)	
 
   if request.method == 'GET':
-    initial = { 'name': user.name, 'about': user.about, 'icbm': user.icbm }
-    result = dict(form=UserForm(initial))
-    
+	initial = { 'name': user.name, 'about': user.about, 'icbm': user.icbm }
+	result = dict(form=UserForm(initial))
+	
   elif request.method == 'POST':
-    form = UserForm(request.POST, request.FILES)
-    if form.is_valid():
-      save = False
-      for item in ('name', 'about', 'icbm', 'sex', 'avatar', 'background'):
-        value = form.cleaned_data.get(item, None)
-        if value:
-          setattr(user, item, value)
-          save = True
-      if save:
-        user.save()
-    result = dict(form=form)
-      
+	form = UserForm(request.POST, request.FILES)
+	if form.is_valid():
+	  save = False
+	  for item in ('name', 'about', 'icbm', 'sex', 'avatar', 'background'):
+		value = form.cleaned_data.get(item, None)
+		if value:
+		  setattr(user, item, value)
+		  save = True
+	  if save:
+		user.save()
+	result = dict(form=form)
+	  
   return HTTPResponse(template.render(RequestContext(request, result)))
   
