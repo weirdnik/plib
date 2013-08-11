84658bfb6e84d6dc8fa8f69ef3781e21a34d0528
Alex <alex@plum.me>
2013-08-11 22:54:38 +0100

    importer view enabled

--- blip/views.py
+++ blip/views.py
@@ -1,1 +1,27 @@
 # Create your views here.
+
+
+def importer(request):
+
+  pass
+#def import(request):
+
+#  ''' user = USer
+#  blip_user
+#  blip_password
+#  started
+#  finished
+#  '''
+
+#
+#  if request.method == 'GET':
+#    template = loader.get_template("import.html")
+#    form = models.BlipForm()
+#    return HTTPResponse(template.render(RequestContext(request, dict(form=form))))                                                                                                                                                                                                                            
+#                                                                                                                                                                                                                                return data    
+#  elif request.method == 'POST':
+  
+    
+ #   pass
+    
+
