73a725eb9abbb23ccc386044cad4c3444cf41d0c
Alex <alex@plum.me>
2013-08-11 23:40:14 +0100

    importer  added

--- blip/models.py
+++ blip/models.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
+
+class Blip (models.Model):
+  user = models.ForeignKey('profile.User', null=False, blank=False)
+  blip = models.TextField(null=False, blank=False)
+  password = models.TextField(null=False, blank=False)
+  imported = models.BooleanField (default=False)  
+
+class Info (models.Model):
+  type = models.TextField(null=False, blank=False)
+  transport = models.TextField(null=False, blank=False)
+  likes = models.IntegerField(default=0)
+#  liked =   userzy
+
+class BlipForm (ModelForm):
+  class Meta:
+    model = Blip
+    fields = ['blip', 'password']
+                        
--- blip/views.py
+++ blip/views.py
@@ -1,27 +1,34 @@
 # Create your views here.
+# -*- coding: utf-8 -*-
+
+from models import BlipForm
+
+from django.http import HttpResponse as HTTPResponse
+from django.http import HttpResponseRedirect as HTTPResponseRedirect
+from django.http import HttpResponseGone as HTTPResponseGone
+from django.http import HttpResponseBadRequest as HTTPResponseBadRequest
+from django.http import HttpResponseNotAllowed as HTTPResponseNotAllowed
+from django.http import HttpResponseForbidden as HTTPResponseForbidden
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.template import RequestContext, loader, Template
 
 
+@login_required
 def importer(request):
 
-  pass
-#def import(request):
+  '''This view accepts data from the import blip account form and puts them
+  in the database for the importer to use.
+  '''
 
-#  ''' user = USer
-#  blip_user
-#  blip_password
-#  started
-#  finished
-#  '''
+  if request.method == 'GET':
+    template = loader.get_template("import.html")
+    form = BlipForm()
+    return HTTPResponse(template.render(RequestContext(request, dict(form=form))))                                                                                                                                                                                                                            
 
-#
-#  if request.method == 'GET':
-#    template = loader.get_template("import.html")
-#    form = models.BlipForm()
-#    return HTTPResponse(template.render(RequestContext(request, dict(form=form))))                                                                                                                                                                                                                            
-#                                                                                                                                                                                                                                return data    
 #  elif request.method == 'POST':
   
     
- #   pass
     
 
