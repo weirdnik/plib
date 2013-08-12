a8480961628a8bb257ac0cf86adf2d554ccf70bc
Alex <alex@plum.me>
2013-08-12 13:17:33 +0200

    app_labels added for interactive running

--- blip/models.py
+++ blip/models.py
@@ -10,11 +10,23 @@ class Blip (models.Model):
   blip = models.TextField(null=False, blank=False)
   password = models.TextField(null=False, blank=False)
   imported = models.BooleanField (default=False)  
+  slug = models.SlugField(max_length=64) # security slug
+  class Meta:
+    app_label = 'blip'
+
 
 class Info (models.Model):
+  blip = models.IntegerField()
   type = models.TextField(null=False, blank=False)
   transport = models.TextField(null=False, blank=False)
   likes = models.IntegerField(default=0)
+  
+  def __unicode__(self):
+    return ':'.join([ str(i) for i in (self.id, self.blip, self.transport)])
+
+  class Meta:
+    app_label = 'blip'
+      
 #  liked =   userzy
 
 class BlipForm (ModelForm):
