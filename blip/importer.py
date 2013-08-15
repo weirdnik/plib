cd27935a31edd59d441d4e2f76771b9ed126bd7b
Janusz A. Urbanowicz <alex@hell.pl>
2013-08-15 14:38:31 +0200

    importer works

--- blip/importer.py
+++ blip/importer.py
@@ -7,10 +7,16 @@ import os
 import sys
 import re
 import json
+import subprocess
+
+global WORKDIR
 
 DEBUG = True
-
+RUN = '/var/tmp'
 WORKDIR = '/srv/www/plib.hell.pl/plib'
+SRC_DIR = (lambda p:'/'.join(p.split('/')[:-1]))(os.path.abspath(__file__))
+
+JOBS = 2
 
 RE = re.compile('^blip-\w+-(?P<file_id>\d+)-full.txt$')
 
@@ -93,32 +99,40 @@ def update_account(username, blipname):
     # updating the avatar
     
     avatar_filename = avatar(username)
+    
     if avatar_filename:
       profile.avatar.save(avatar_filename, File(file(avatar_filename)))
+  
+    tags_filename =  os.path.join(datadir(),'tags-%s.txt' % username)
+    print tags_filename   
+    if os.path.exists(tags_filename):
+      subscribed_tags = json.loads(file(tags_filename, 'r').read())
+      for t in subscribed_tags:
+        tag_path = t.get('tag_path', None)
+        if tag_path:
+          tag_text = tag_path.split('/')[-1].lower()
+          print tag_text
+          tag, create = Tag.objects.get_or_create(tag=tag_text)
+          if create:
+            tag.save()
+          profile.watches_tags.add(tag)
+      profile.save()
 
-    subscribed_tags = json.loads(file(os.path.join(datadir(),'tags-%s.txt' % username), 'r').read())
-    for t in subscribed_tags:
-      tag_path = t.get('tag_path', None)
-      if tag_path:
-        tag_text = tag_path.split('/')[-1].lower()
-        print tag_text
-        tag, create = Tag.objects.get_or_create(tag=tag_text)
-        if create:
-          tag.save()
-        profile.watches_tags.add(tag)
-    profile.save()
+    tags_filename = os.path.join(datadir(),'tags-ignored-%s.txt' % username)
+    print tags_filename   
+    if os.path.exists(tags_filename):
 
-    ignored_tags = json.loads(file(os.path.join(datadir(),'tags-ignored-%s.txt' % username), 'r').read())
-    for t in ignored_tags:
-      tag_path = t.get('tag_path', None)
-      if tag_path:
-        tag_text = tag_path.split('/')[-1].lower()
-        print tag_text
-        tag, create = Tag.objects.get_or_create(tag=tag_text)
-        if create:
-          tag.save()
-        profile.ignores_tags.add(tag)
-    profile.save()
+      ignored_tags = json.loads(file(tags_filename, 'r').read())
+      for t in ignored_tags:
+        tag_path = t.get('tag_path', None)
+        if tag_path:
+          tag_text = tag_path.split('/')[-1].lower()
+          print tag_text
+          tag, create = Tag.objects.get_or_create(tag=tag_text)
+          if create:
+            tag.save()
+          profile.ignores_tags.add(tag)
+      profile.save()
 
   from django.shortcuts import get_object_or_404
   from django.contrib.auth.models import User as User
@@ -145,10 +159,6 @@ def update_account(username, blipname):
 
 def check_running():
   '''test if other instances are running'''
-  
-  RUN = '/var/tmp'
-  
-
 
   files = [ f for f in os.listdir(RUN) if f.startswith('blip.') ]
 
@@ -167,15 +177,84 @@ def check_running():
     else:
       blipname = f.split('.')[1]
       blips.append(blipname)
-      
+
+  print blips      
   return blips
 
+def queue():
+  
+  from models import Blip
+    
+  blips =  [ (i.blip, i) for i in Blip.objects.filter(imported__exact=False)]
+    
+  print 'q,', blips
+  return blips
+  
+  
+def robmar(blipname, blippassword):
+
+  import stat
+
+  if DEBUG:
+    print 'Importing %s...' % blipname
+    
+  ROBMAR = 'get-blip-archive.sh'
+  
+  from tempfile import mkdtemp
+  from shutil import copy
+  
+  tmpdir = mkdtemp(suffix=blipname)
+  
+  copy(os.path.join(SRC_DIR, ROBMAR), tmpdir)
+  
+  os.chdir(tmpdir)
+  try:
+    os.chmod(ROBMAR, stat.S_IRUSR| stat.S_IWUSR | stat.S_IXUSR)
+    result = subprocess.check_output(['bash','./%s' % ROBMAR, ':'.join((blipname, blippassword))])
+    print result
+  except subprocess.CalledProcessError:
+    # os.rmtree(tmpdir)
+    print result
+    
+    # TODO zipping
+    
+  return tmpdir
+
+  
 if __name__ == '__main__':
 
   file('/var/tmp/blip.test','w').write(str(os.getpid()))
-  print check_running()
+  
+  r = set(check_running())
 
-  mypid = os.getpid()
+  print r  
+  if len(r) < JOBS+2:
+    blips = queue()
+    q = set([i[0] for i in blips])
+    print q
+    waiting=list(q-r)
+
+        
+    if waiting:
+      job = dict(blips).get(waiting[0])    
+      username = job.user.user.username
+      blipname = job.blip
+      pid = os.getpid()
+      pidfilename = os.path.join(RUN, 'blip.%s' % blipname)
+      file(pidfilename, 'w').write(str(pid))
+      
+      WORKDIR = robmar(blipname, job.password)
+          
+      print WORKDIR
+      
+      update_account (username, blipname)
+      #os.remove(pidfilename)
+      
+      # TODO zipping
+      
+  
+
+  
 
   print "This file should be run as a part of PLIB."
 #  update_account('alex', 'alex')
