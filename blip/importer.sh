f1f280640b8515a3483b1a14e00099491788f4f3
Alex <alex@hell.pl>
2013-08-27 16:38:10 +0100

    fixes

--- blip/importer.sh
+++ blip/importer.sh
@@ -1,5 +1,5 @@
 
-export PYTHONPATH=/srv/www/plib.hell.pl/plib
+export PYTHONPATH=/srv/www/plum.hell.pl/plib
 export DJANGO_SETTINGS_MODULE=settings
 
-python /srv/www/plib.hell.pl/plib/blip/importer.py
+python /srv/www/plum.hell.pl/plib/blip/importer.py
