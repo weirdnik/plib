58b8f65e44a2523bc0ea75f7d2065bd94a7eea14
Alex <alex@hell.pl>
2013-09-02 23:08:33 +0100

    bliplogs work

--- profile/views.py
+++ profile/views.py
@@ -78,7 +78,7 @@ def unfollow (request, username):
 	return HTTPResponseNotAllowed ()
 
 
-def blog (request, username, year=None, month=None, week=None, day = None):
+def blog (request, username, year=None, month=None, week=None, day=None):
 
   user = get_object_or_404(User, user__username__exact=username)
   statuses = Status.objects.filter(owner__exact=user, recipient__exact=None,
@@ -90,7 +90,7 @@ def blog (request, username, year=None, month=None, week=None, day = None):
     year=str(d.year)
     today=True
   if not week:
-    week=str(d.isocalendar()[1])
+    week=str(d.isocalendar()[1]-1)
     today=True
   template = loader.get_template('blog.html')
 
