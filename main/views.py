feb38eb651536b7924392d1a0c37cfad6aa0621e
Alex <alex@hell.pl>
2013-09-04 15:39:47 +0100

    most popular statuses on a frontpage

--- main/views.py
+++ main/views.py
@@ -8,6 +8,8 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 from django.db.models import Count
 
+import datetime
+
 from cockpit.models import Status, Like
 
 from settings import APP_NAME
@@ -15,22 +17,18 @@ from settings import APP_NAME
 def front(request):
 
   if request.method == 'GET':
+
+    now = datetime.datetime.now()
+    week = datetime.timedelta(days=7)
+    
+    likes = Status.objects.filter(date__gte=(now-week)).annotate(likes_count=Count('likes')).order_by('-likes_count')[:16]
   
     statuses = Status.objects.filter(private__exact=False, recipient__exact=None).order_by('-date')    
     feed = statuses[:20] if statuses else None
     
-    now = datetime.date.today()
-    week = datetime.timedelta(weeks=1)
-    since = now-week
-    
-    # this is bad
-    liked = Like.objects.filter(status__private__exact=False, 
-      status__recipient__exact=None, 
-      status__date__gte=since).annotate(likes=Count('user')).order_by('likes')
-    likes = liked[:10] if liked else None
-
     template = loader.get_template('front.html')
-    return HTTPResponse(template.render(RequestContext(request, dict(feed=feed,likes=likes,app_name=APP_NAME))))
+    return HTTPResponse(template.render(RequestContext(request,
+      dict(feed=feed,likes=likes,app_name=APP_NAME))))
       
 def health(request):
 
