c038e91a715c29b9fb7c26b0a585333e7eff139f
Alex <alex@plum.me>
2013-09-07 09:42:13 +0100

    status fixes

--- cockpit/views.py
+++ cockpit/views.py
@@ -235,7 +235,7 @@ def status (request, object_id=None, mobile=False):
           tag_result = TAG_RE.findall(status.text)
           status.tagged = True if tag_result else False
 
-          status.text = escape(status.text) # SECURITY !!!!! DO NOT REMOVE!!!
+          status.text = escape(status.text.strip()) # SECURITY !!!!! DO NOT REMOVE!!!
           status.save()        
       except UnicodeEncodeError:
         return HTTPResponseRedirect(reverse('message_dashboard',
