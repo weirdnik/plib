7b78b0343408ec6387c1078054e377da625fd0af
root <root@inferno.hell.pl>
2014-03-25 22:34:17 +0100

    youtube re fix

--- cockpit/models.py
+++ cockpit/models.py
@@ -16,13 +16,13 @@ STATUS_LENGTH = 160
 TAG_RE = re.compile(ur'(?:\s|\A)#(?P<tag>\w+)', re.UNICODE)
 MENTION_RE = re.compile('\^(?P<username>\w+)')
 
-YOUTUBE_RE = re.compile ('https?://(www.)?(youtu.be/|youtube.com/watch\?(?:feature=player_embedded&)?v=)(?P<video>[\w\d-]+)(?:#t=\d+)?')
+YOUTUBE_RE = re.compile ('https?://(www.)?((?:youtu.be/)|(?:youtube.com/watch\?(?:feature=player_embedded&(?:amp;)?)?v=))(?P<video>[\w\d-]+)(?:#t=\d+)?(?:&feature=youtu.be)?')
 VIMEO_RE = re.compile ('https?://(www.)?vimeo.com/(?P<video>[\w\d]+)')
 INSTAGRAM_RE = re.compile('https?://instagram.com/p/(?P<image>[\w\d]+)/?')
 
 # the URL clause finds urls not preceded by iframe call to not fuck embeds - FUGLY
 
-PROCESS_RE = re.compile('(?P<YT>https?://(www.)?(youtu.be/|youtube.com/watch\?(?:feature=player_embedded&)?v=)(?P<YT_id>[\w\d-]+)(?:#t=\d+)?)|(?P<Vimeo>https?://(www.)?vimeo.com/(?P<V_ID>[\w\d]+))|(?P<Instagram>https?://instagram.com/p/(?P<I_ID>[\w\d]+)/?)|(?<!(<iframe src=")|(t="270" src="))(?P<url>https?://[\w-]+(\.[\w-]+)*(/[\w\?=,.\-%&;#]*)*)')
+PROCESS_RE = re.compile('(?P<YT>https?://(www.)?((?:youtu.be/)|(?:youtube.com/watch\?(?:feature=player_embedded&(?:amp;)?)?v=))(?P<video>[\w\d-]+)(?:#t=\d+)?(?:&feature=youtu.be)?)|(?P<Vimeo>https?://(www.)?vimeo.com/(?P<V_ID>[\w\d]+))|(?P<Instagram>https?://instagram.com/p/(?P<I_ID>[\w\d]+)/?)|(?<!(<iframe src=")|(t="270" src="))(?P<url>https?://[\w-]+(\.[\w-]+)*(/[\w\?=,.\-%&;#]*)*)')
 
 MESSAGE_RE = re.compile('^(\>|&gt;)(\>|&gt;)?(?P<recipient>\w+):?')
 MSG_PREFIX_RE = re.compile('^\>')
