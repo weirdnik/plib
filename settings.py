47576cbc699474ec462ed2c68dc74726103ccee8
Alex <alex@plum.me>
2016-11-12 18:35:55 +0100

    template settings updated

--- settings.py
+++ settings.py
@@ -96,8 +96,27 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR,'templates'),
+    os.path.join(PROJECT_DIR, 'templates'),
 )
+
+TEMPLATES = [ {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': TEMPLATE_DIRS,
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+            # list if you haven't customized them:
+            'django.contrib.auth.context_processors.auth',
+            'django.template.context_processors.debug',
+            'django.template.context_processors.i18n',
+            'django.template.context_processors.media',
+            'django.template.context_processors.static',
+            'django.template.context_processors.tz',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+  },]
 
 INSTALLED_APPS = (
     'django.contrib.auth',
